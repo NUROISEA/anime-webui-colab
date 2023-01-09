afc70b673bae5759bce5bfcdb5c52d0f2e5900b7
NUROISEA <120075289+NUROISEA@users.noreply.github.com>
2023-01-09 15:47:22 +0800

    Add links to a list on utility script

--- configs/utility.py
+++ configs/utility.py
@@ -8,3 +8,25 @@ def dictionary_to_json(json_file, data_dictionary):
 
     with open(json_file, 'w') as f:
         json.dump(json_data, f)
+
+has_run = False
+
+models_downloaded = []
+
+default_extensions = [
+    "https://github.com/yfszzx/stable-diffusion-webui-images-browser",
+    "https://github.com/DominikDoom/a1111-sd-webui-tagcomplete",
+]
+
+default_embeddings = [
+    "https://huggingface.co/nick-x-hacker/bad-artist/resolve/main/bad-artist.pt",
+    "https://huggingface.co/datasets/Nerfgun3/bad_prompt/resolve/main/bad_prompt_version2.pt",
+]
+
+default_configs = [
+    "https://github.com/NUROISEA/anime-webui-colab/raw/main/configs/ui-config.json",
+    "https://github.com/NUROISEA/anime-webui-colab/raw/main/configs/config.json",
+    "https://github.com/NUROISEA/anime-webui-colab/raw/main/configs/styles.csv",
+]
+
+xformers_link = "https://github.com/camenduru/stable-diffusion-webui-colab/releases/download/0.0.15/xformers-0.0.15.dev0+189828c.d20221207-cp38-cp38-linux_x86_64.whl"
